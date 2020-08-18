ece0be1aa4f480b44fbf2205b3e3b18a0794f0c7
Borodor <deepanshuwadhwa97@gmail.com>
2020-08-18 06:34:52 -0400

    Tweaking and cleaning up

--- custom_smoothness/map_generator.py
+++ custom_smoothness/map_generator.py
@@ -53,7 +53,8 @@ class MapGen:
         # Mean map
         self.xbar = np.mean(np.nanmean(self.real_maps, axis=1), axis=0)
         self.real_maps = self.real_maps - self.xbar
-        # Variance matrix
+
+        # Variance array
         subject_var = []
         for subject in self.real_maps:
             subject_var.append(np.nanmean((np.nanmean(subject, axis=0) -
@@ -73,7 +74,7 @@ class MapGen:
         subject_component: subject specific component
         finger_component: array with the finger components
         noise_component: noise component for each finger
-        noise_visualize: noise maps converted to pixels
+        noise_pixel: noise components in the pixel space
         data_dict: dictionary consisting of the above listed components
         """
 
@@ -86,33 +87,38 @@ class MapGen:
         noise_mat = mappingFile['noise_corr']
         avg_Bvar_est = mappingFile['avg_Bvar_est']
         del mappingFile
-        print ("Subject")
 
         # Generate Subject specific component
+        print ("Subject")
         subject_component = np.mean(subject, axis = 0)
-        subject_covariance_matrix = np.dot((subject_component.reshape(16384, 1) -
+        subject_covariance_matrix = np.dot((subject_component.reshape(-1, 1) -
                                             subject_component.mean()),
-                                           (subject_component.reshape(1, 16384) -
+                                           (subject_component.reshape(1, -1) -
                                             subject_component.mean())) + np.eye(16384) * 0.0000001
+        
+        # Use cholesky decomposition floowed by matrix multiplication to
+        # generate the component with the required correlation structure
+
         a = chol(subject_covariance_matrix)
         z = np.random.normal(0, 1, size = (16384,))
         z = z/np.std(z)
         subject_component = np.dot(a, z)
         subject_component = (subject_component/subject_component.std()) * (np.sqrt(self.var_s[sub_index]))
-        print (subject_component.mean(), subject_component.min(), subject_component.max(), subject_component.std())
-        subject_covariance_matrix = np.dot((subject_component.reshape(16384, 1) -
-                                            subject_component.mean()),
-                                           (subject_component.reshape(1, 16384) -
-                                            subject_component.mean())) + np.eye(16384) * 0.0000001
-        print ("Finger")
-        # Generate finger specific components
 
+        
+        # Generate finger specific components
+        print ("Finger")
+        subject_covariance_matrix = np.dot((subject_component.reshape(-1, 1) -
+                                            subject_component.mean()),
+                                           (subject_component.reshape(1, -1) -
+                                            subject_component.mean())) + np.eye(16384) * 0.0000001
         finger_covariance_matrix = \
             np.ma.cov(np.ma.masked_invalid(subject - subject.mean(axis=0))) + 0.0000001
         finger_component = mnn.rvs(rowcov=finger_covariance_matrix,
                                    colcov=subject_covariance_matrix)
-        print ("Noise")
+        
         # Generate noise
+        print ("Noise")
         noise_list = []
         pixel_noise_list = []
         try:
@@ -129,7 +135,7 @@ class MapGen:
             "subject_component": subject_component,
             "finger_component": finger_component,
             "noise_component": noise_list,
-            "noise_visualize": pixel_noise_list
+            "noise_component": pixel_noise_list
         }
         del noise_list, finger_component, pixel_noise_list, noise_mat, subject_covariance_matrix
         return data_dict
@@ -142,7 +148,6 @@ class MapGen:
         """
         print ("Make Maps")
         generated_maps = []
-        #self.real_maps = self.real_maps - self.xbar
         for i in range(0, self.real_maps.shape[0]):
             maps_list = []
             data_dict = self.calculate_map_params(self.real_maps[i],
@@ -151,6 +156,7 @@ class MapGen:
                 continue
 
             # Load pixel and voxel transformation matrices
+
             matrix_dir = self.data_dir + "/" + self.subject_dirs[i] + "/"
             mappingFile = matrix_dir + self.subject_dirs[i] + "." + "voxelMappingInfo.pkl"
             with open(mappingFile, 'rb') as mfile:
@@ -159,30 +165,37 @@ class MapGen:
             vox2pix = np.array(csr_matrix(mappingFile['vox2Pixel']).todense())
             pix2vox = np.array(csr_matrix(mappingFile['pixel2Vox']).todense())
             del mappingFile
+
             # Combine xbar and the subject component
-            print ("S")
+
             subject_map = self.xbar.reshape(128, 128).T + data_dict['subject_component'].reshape(128, 128)
             true_maps = []
-            # Iterate over the finger components and generated the map for each finger
+
+            # Iterate over the finger components and generate the map for each finger
+
             for finger in range(0, 5):
                 pixel_map = subject_map + data_dict['finger_component'][finger].reshape(128, 128)
+                voxel_map = np.dot(pix2vox.T, pixel_map.flatten().reshape(-1, 1))
+                true_map = pixel_map
+                true_maps.append(true_map.flatten())
+                voxel_map = voxel_map + data_dict['noise_component'][finger]
+                new_map = np.dot(vox2pix.T, voxel_map.flatten())
+                maps_list.append(new_map.flatten())
+
+                # Store the component and map variances in the dictionary
+
                 self.var[self.subject_dirs[i]] = self.var_style
                 self.var[self.subject_dirs[i]]["components"].append([self.xbar.var(), data_dict['subject_component'].var(),
                                                data_dict['finger_component'][finger].var()])
                 self.var[self.subject_dirs[i]]["true_pixel"].append(pixel_map.var())
-                voxel_map = np.dot(pix2vox.T, pixel_map.flatten().reshape(-1, 1))
                 self.var[self.subject_dirs[i]]["true_voxel"].append(voxel_map.var())
                 self.var[self.subject_dirs[i]]["noise_voxel"].append(data_dict['noise_component'][finger].var())
-                #true_map = np.dot(vox2pix.T, voxel_map.flatten())
-                true_map = pixel_map
-                true_maps.append(true_map.flatten())
-                voxel_map = voxel_map + data_dict['noise_component'][finger]
                 self.var[self.subject_dirs[i]]["voxel_map"].append(voxel_map.var())
-                new_map = np.dot(vox2pix.T, voxel_map.flatten())
-                maps_list.append(new_map.flatten())
-
+                
             data_dict['maps'] = maps_list
             data_dict['true_maps'] = true_maps
+
+            # Save the generated maps
             print ("Save")
             self.save_map(data_dict, self.subject_dirs[i], save_dir)
             generated_maps.append(maps_list)
@@ -199,26 +212,25 @@ class MapGen:
         :return: None
         """
         subject_save_dir = save_dir + str(subject_index) + "/"
-        subject_matrices_dir = subject_save_dir + "matrices/"
         subject_maps_dir = subject_save_dir + "maps/"
-        true_maps_dir = subject_save_dir + "t_maps/"
         if not os.path.exists(subject_maps_dir):
             os.makedirs(subject_maps_dir)
-        #if not os.path.exists(subject_matrices_dir):
-            #os.makedirs(subject_matrices_dir)
         matrix_loc = subject_maps_dir + "generatedMaps.pkl"
         with open(matrix_loc, mode = "wb") as f:
             pkl.dump(data_dict['maps'], f)
+
         matrix_loc = subject_maps_dir + "trueMaps.pkl"
         with open(matrix_loc, mode = "wb") as f:
             pkl.dump(data_dict['true_maps'], f)
+
         subject_maps_dir1 = subject_save_dir + "maps_imgs/"
         if not os.path.exists(subject_maps_dir1):
             os.makedirs(subject_maps_dir1)
         for i in range(0, 5):
             imageio.imsave((subject_maps_dir1 + str(i) + ".png"), data_dict['maps'][i].reshape(128, 128).T)
             print((subject_maps_dir + str(i) + ".png"))
-            subject_maps_dir2 = subject_save_dir + "maps_true/"
+            
+        subject_maps_dir2 = subject_save_dir + "maps_true/"
         if not os.path.exists(subject_maps_dir2):
             os.makedirs(subject_maps_dir2)
         for i in range(0, 5):
